686972fcec1ccc9aa2e6573f7ac3f3aef691798c
Rubyroy12 <ibrakipz7@gmail.com>
2023-03-19 13:51:07 +0300

    add collection updates

---
+++ src/app/staff/sales/pages/float-allocation/float-allocation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-float-allocation',
+  templateUrl: './float-allocation.component.html',
+  styleUrls: ['./float-allocation.component.sass']
+})
+export class FloatAllocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
