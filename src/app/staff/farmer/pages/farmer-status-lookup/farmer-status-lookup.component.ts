bdebba0c15e63f00557d540dab85e8a4a117e5ef
NdiranguMichaelEMTH <mndirangu@emtechhouse.co.ke>
2025-04-23 13:01:28 +0300

    Update farmer service to fetch active route and mcc farmers

--- src/app/staff/farmer/pages/farmer-status-lookup/farmer-status-lookup.component.ts
+++ src/app/staff/farmer/pages/farmer-status-lookup/farmer-status-lookup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FarmerService } from '../../services/farmer.service';
+import { NotificationService } from 'src/app/data/services/notification.service';
 
 @Component({
   selector: 'app-farmer-status-lookup',
@@ -7,9 +9,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FarmerStatusLookupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: FarmerService, private snackbar: NotificationService) { }
+  farmers: any[] = []
+  routeId: number
+  months: number
+  locationId: number
+  loading: boolean = false
 
   ngOnInit(): void {
+
+  }
+
+  private getAllActiveFarmers():void {
+    this.loading = true
+
+    this.service.getActiveFarmers().subscribe({
+      next: (res) => {
+        this.loading = false;
+
+        if (res.entity.length > 0) {
+          this.farmers = res.entity
+        } else {
+          this.snackbar.alertWarning("No active farmers found")
+        }
+      },
+      error: (err) => {
+        this.loading = false
+
+        this.snackbar.alertWarning(err)
+        console.error(err)
+      },
+      complete: () => {}
+    })
+  }
+
+
+  private getRouteActiveFarmers(routeId: number, months: any):void {
+    this.loading = true
+
+    this.service.getRouteActiveFarmers(routeId, months).subscribe({
+      next: (res) => {
+        this.loading = false;
+
+        if (res.entity.length > 0) {
+          this.farmers = res.entity
+        } else {
+          this.snackbar.alertWarning("No active farmers found")
+        }
+      },
+      error: (err) => {
+        this.loading = false
+
+        this.snackbar.alertWarning(err)
+        console.error(err)
+      },
+      complete: () => {}
+    })
+  }
+
+
+  private getCenterActiveFarmers(locationId: number, months: number):void {
+    this.loading = true
+
+    this.service.getCenterActiveFarmers(locationId, months).subscribe({
+      next: (res) => {
+        this.loading = false;
+
+        if (res.entity.length > 0) {
+          this.farmers = res.entity
+        } else {
+          this.snackbar.alertWarning("No active farmers found")
+        }
+      },
+      error: (err) => {
+        this.loading = false
+
+        this.snackbar.alertWarning(err)
+        console.error(err)
+      },
+      complete: () => {}
+    })
   }
 
 }
