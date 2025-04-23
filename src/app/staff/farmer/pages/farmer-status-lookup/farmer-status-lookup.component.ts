1dd0d2c2bd94387a0854174656526f5a0436e766
michaelndirangu <ndirangumichael33@gmail.com>
2025-04-23 15:49:36 +0300

    Farmer lookup based on activeness. methods, dialog. 23/04/25

--- src/app/staff/farmer/pages/farmer-status-lookup/farmer-status-lookup.component.ts
+++ src/app/staff/farmer/pages/farmer-status-lookup/farmer-status-lookup.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FarmerService } from '../../services/farmer.service';
 import { NotificationService } from 'src/app/data/services/notification.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BulkComponent } from 'src/app/staff/sms/bulk/bulk.component';
 
 @Component({
   selector: 'app-farmer-status-lookup',
@@ -8,16 +13,39 @@ import { NotificationService } from 'src/app/data/services/notification.service'
   styleUrls: ['./farmer-status-lookup.component.sass']
 })
 export class FarmerStatusLookupComponent implements OnInit {
-
-  constructor(private service: FarmerService, private snackbar: NotificationService) { }
   farmers: any[] = []
+  dataSource: MatTableDataSource<any>
   routeId: number
   months: number
   locationId: number
   loading: boolean = false
+  isdata: boolean = false
+
+  selected = "";
+  selection = new SelectionModel<any>(true, []);
+
+  displayedColumns: string[] = [
+    'select',
+    'id',
+    "username",
+    "farmer_no",
+    "mobile_no",
+    "ID No.",
+  ];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private service: FarmerService, private snackbar: NotificationService, private dialogRef: MatDialogRef<BulkComponent>) { }
 
   ngOnInit(): void {
 
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   private getAllActiveFarmers():void {
