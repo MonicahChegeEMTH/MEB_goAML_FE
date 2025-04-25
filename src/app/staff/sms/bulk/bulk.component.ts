88a16ee2cab220bb28961d8be1c420f4e9281cda
michaelndirangu <ndirangumichael33@gmail.com>
2025-04-25 16:52:16 +0300

    Filter sms notifications. 25/04/25

--- src/app/staff/sms/bulk/bulk.component.ts
+++ src/app/staff/sms/bulk/bulk.component.ts
@@ -10,75 +10,87 @@ import { SnackbarService } from 'src/app/shared/snackbar.service';
 import { SmsService } from '../sms.service';
 import { InitiateBulkSmsComponent } from '../initiate-bulk-sms/initiate-bulk-sms.component';
 import { DatePipe } from '@angular/common';
-import { ComponentsModule } from "../../../shared/components/components.module";
-import { BreadcrumbComponent } from "../../../shared/components/breadcrumb/breadcrumb.component";
+import { ComponentsModule } from '../../../shared/components/components.module';
+import { BreadcrumbComponent } from '../../../shared/components/breadcrumb/breadcrumb.component';
 import { FarmerStatusLookupComponent } from '../../farmer/pages/farmer-status-lookup/farmer-status-lookup.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { get } from 'http';
 
 @Component({
   selector: 'app-bulk',
   templateUrl: './bulk.component.html',
-  styleUrls: ['./bulk.component.sass'],
+  styleUrls: ['./bulk.component.scss'],
 })
-
 export class BulkComponent implements OnInit {
-  selected:any;
-  templates:any;
+  selected: any;
+  templates: any;
   displayedColumns: string[] = [
-    "id",
-    "messageId",
-    "phoneNumber",
-    "sentDate",
-    "status",
-    "statusReason",
-    "message"
+    'id',
+    'messageId',
+    'phoneNumber',
+    'sentDate',
+    'status',
+    'statusReason',
+    'message',
   ];
 
   dataSource!: MatTableDataSource<any>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   contextMenu: MatMenuTrigger;
-  contextMenuPosition = { x: "0px", y: "0px" };
+  contextMenuPosition = { x: '0px', y: '0px' };
 
   selection = new SelectionModel<any>(true, []);
   data: any;
   error: any;
   isLoading: boolean = true;
   currentUser: any;
-  from: string
-  to: string
-  hasData: boolean = false
+  from: string;
+  to: string;
+  hasData: boolean = false;
+  filterForm: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     public dialog: MatDialog,
     private router: Router,
-    private snackbar:SnackbarService,
+    private snackbar: SnackbarService,
     private service: SmsService,
-    private datePipe: DatePipe,
-  ) { }
+    private datePipe: DatePipe
+  ) {}
 
   ngOnInit(): void {
-    this.from = this.datePipe.transform(new Date(), 'yyyy-MM-dd')
-    this.to = this.datePipe.transform(new Date(), 'yyyy-MM-dd') 
+    this.from = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    this.to = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
 
-    this.getData();
+    this.getData(this.from, this.to);
     this.getBulkCodes();
+
+    this.filterForm = this.fb.group({
+      from: ['', [Validators.required]],
+      selected: ['', [Validators.required]],
+      to: ['', [Validators.required]],
+    });
   }
 
-  getSelected() {
-    this.service.getBulkSMSByCode(this.selected).subscribe(
-      (res) => {
-        this.data = res;
-        if (this.data != null) {
-          this.isLoading = false;
-          this.dataSource = new MatTableDataSource<any>(this.data);
-          this.dataSource.paginator = this.paginator;
-          this.dataSource.sort = this.sort;
-        }
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+  getTodaysData() {
+    if (this.selected == 'today') {
+      this.getData(this.from, this.to);
+    }
+  }
+
+  filterFarmers() {
+    if (this.selected == 'sd') {
+      this.getData(
+        this.datePipe.transform(this.filterForm.value.from, 'yyyy-MM-dd'),
+        this.datePipe.transform(this.filterForm.value.from, 'yyyy-MM-dd')
+      );
+    } else if (this.selected == 'dr') {
+      this.getData(
+        this.datePipe.transform(this.filterForm.value.from, 'yyyy-MM-dd'),
+        this.datePipe.transform(this.filterForm.value.to, 'yyyy-MM-dd')
+      );
+    }
   }
 
   getBulkCodes() {
@@ -93,39 +105,37 @@ export class BulkComponent implements OnInit {
   }
 
   refresh() {
-    this.getData();
+    this.getData(this.from, this.to);
   }
 
-  getData() {
+  getData(from: string, to: string) {
     this.isLoading = true;
-    this.service.getByDateRange(this.from, this.to).subscribe({
+    console.log('from and to is ', from, to);
+    this.service.getByDateRange(from, to).subscribe({
       next: (res) => {
         this.data = res.entity;
         if (this.data != null && this.data.length > 0) {
           this.isLoading = false;
-          this.hasData  = true
+          this.hasData = true;
           this.dataSource = new MatTableDataSource<any>(this.data);
           this.dataSource.paginator = this.paginator;
           this.dataSource.sort = this.sort;
         } else {
           this.isLoading = false;
-          this.hasData  = false
+          this.hasData = false;
         }
       },
       error: (err) => {
         this.isLoading = false;
-        this.hasData  = false
+        this.hasData = false;
         console.log(err);
-      }
-    }
-    );
+      },
+    });
   }
 
-  readMessage(message)
-  {
-    this.snackbar.showNotification("snackbar-success", message);
+  readMessage(message) {
+    this.snackbar.showNotification('snackbar-success', message);
   }
-
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -137,20 +147,22 @@ export class BulkComponent implements OnInit {
 
   addCall() {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = false
-    dialogConfig.autoFocus = true
-    dialogConfig.width = "60%"
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '60%';
     dialogConfig.data = {
-      test: ""
-    }
-    this.dialog.open(FarmerStatusLookupComponent, dialogConfig).afterClosed().subscribe({
-      next: (res) => {
-        this.getData()
-      },
-      complete: () => {
-        this.getData()
-      }
-    })
+      test: '',
+    };
+    this.dialog
+      .open(FarmerStatusLookupComponent, dialogConfig)
+      .afterClosed()
+      .subscribe({
+        next: (res) => {
+          this.getData(this.from, this.to);
+        },
+        complete: () => {
+          this.getData(this.from, this.to);
+        },
+      });
   }
-
 }
