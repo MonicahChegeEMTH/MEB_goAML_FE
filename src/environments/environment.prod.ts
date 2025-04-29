0fa5e4d2f6cb6acc306b8f597584fa24086bdd1e
michaelndirangu <ndirangumichael33@gmail.com>
2025-04-29 15:22:51 +0300

    Update server url to 9400 from 8600. Blocked internally. 29/04/25

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://18.219.121.50:8600",
-  API: "http://18.219.121.50:8600",
+  apiUrl: "http://18.219.121.50:9400",
+  API: "http://18.219.121.50:9400",
 };
