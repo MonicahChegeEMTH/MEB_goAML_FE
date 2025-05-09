8c9f974613d0e9607af6a9f9206764844494a2eb
michaelndirangu <ndirangumichael33@gmail.com>
2025-05-09 14:58:33 +0300

    Update server base url to https, remove # from routes. 09/05/25

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://18.219.121.50:9400",
-  API: "http://18.219.121.50:9400",
+  apiUrl: "https://dairy.emtechhouse.co.ke:8800",
+  API: "https://dairy.emtechhouse.co.ke:8800",
 };
