453d680dc6831b51cd148fbbf45431258274af13
MoniqChege <quichege33@gmail.com>
2025-10-22 11:54:08 +0300

    the second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,14 @@
 
 export const environment = {
   production: false,
+  apiUrl: "http://18.219.121.50:9400",
+  API: "http://18.219.121.50:9400",
   // apiUrl: "http://localhost:9600",
   // API: "http://localhost:9600",
 //   apiUrl: "http://172.16.9.84:9600",
 //   API: "http://172.16.9.84:9600",
-  apiUrl: "http://172.16.10.215:9600",
-  API: "http://172.16.10.215:9600",
+//   apiUrl: "http://172.16.10.215:9600",
+//   API: "http://172.16.10.215:9600",
 };
 
 /*
