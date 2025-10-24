af9b42c08849159bfb8696f75158bf1ed5576496
MoniqChege <quichege33@gmail.com>
2025-10-24 17:11:24 +0300

    commit

--- src/app/auditor/logs/logs/logs.component.ts
+++ src/app/auditor/logs/logs/logs.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { TokenStorageService } from 'src/app/core/service/token-storage.service';
 import { AuditLog } from '../service/audit-log.model';
 import { AuditLogService } from '../service/audit-log.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-logs',
@@ -16,6 +17,8 @@ export class LogsComponent implements OnInit {
   errorMessage = '';
   firstname: string;
   lastname: string;
+  searchText: string = '';
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
 
   displayedColumns: string[] = [
     'user_id',
@@ -39,6 +42,16 @@ export class LogsComponent implements OnInit {
     const user = this.tokenStorage.getUser();
     this.firstname = user.firstname;
     this.lastname = user.lastname;
+
+     this.dataSource.filterPredicate = (data, filter) => {
+      const f = filter.trim().toLowerCase();
+      return (
+        data.user_id?.toLowerCase().includes(f) ||
+        data.username?.toLowerCase().includes(f) ||
+        data.action_type?.toLowerCase().includes(f) ||
+        data.report_type?.toLowerCase().includes(f)
+      );
+    };
   }
 
   fetchAuditLogs(): void {
@@ -64,5 +77,19 @@ export class LogsComponent implements OnInit {
 
   private setPagedData(startIndex: number, endIndex: number): void {
     this.pagedReports = this.reports.slice(startIndex, endIndex);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  clearSearch() {
+    this.searchText = '';
+    const event = { target: { value: '' } } as unknown as Event;
+    this.applyFilter(event);
   }
 }
